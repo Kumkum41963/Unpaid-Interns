d838ec72f33216d0c12916b3c58c9a2fb750576e
Kumkum41963 <kumkum19305@gmail.com>
2025-02-13 01:57:29 +0530

    feat: client setup done

---
+++ client/app/(tabs)/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import{View,Text} from 'react-native'
+const About = () => {
+  return (
+   <View>
+    <Text style={{color:'yellow'}}>About</Text>
+   </View>
+  )
+}
+
+export default About
